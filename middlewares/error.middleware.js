75e0a4de5b7cc5723da64dec910c51e9f1de5064
Felipe Wallace <felipewallacepc@gmail.com>
2025-02-18 20:52:14 -0300

    Correções de Bugs

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -3,38 +3,34 @@ const errorMiddleware = (err, req, res, next) => {
     let error = { ...err };
 
     error.message = err.message;
-    console.log(err);
 
+    console.error(err);
 
     // Mongoose bad ObjectId
-    if (err.name === "CastError") {
-      const message = `Resource not found`;
+    if (err.name === 'CastError') {
+      const message = 'Resource not found';
       error = new Error(message);
       error.statusCode = 404;
     }
 
     // Mongoose duplicate key
     if (err.code === 11000) {
-      const message = `Duplicate field value entered`;
+      const message = 'Duplicate field value entered';
       error = new Error(message);
       error.statusCode = 400;
     }
 
     // Mongoose validation error
-    if (err.name === "ValidationError") {
+    if (err.name === 'ValidationError') {
       const message = Object.values(err.errors).map(val => val.message);
-      error = new Error(message.join(", "));
+      error = new Error(message.join(', '));
       error.statusCode = 400;
     }
 
-    res.status(error.statusCode || 500).json({
-      success: false,
-      error: error.message || "Server Error"
-    })
-
+    res.status(error.statusCode || 500).json({ success: false, error: error.message || 'Server Error' });
   } catch (error) {
     next(error);
-  }  
+  }
 };
 
 export default errorMiddleware;
