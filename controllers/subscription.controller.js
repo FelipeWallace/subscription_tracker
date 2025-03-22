aab7cd9f0d6f4b8bdc9e439c692b189fa23c6c06
Felipe Wallace <felipewallacepc@gmail.com>
2025-03-22 18:08:50 -0300

    Adding Reminder Workflow

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,15 +1,30 @@
+import { workflowClient } from "../config/upstash.js";
 import Subscription from "../models/subscription.model.js";
+import { SERVER_URL } from "../config/env.js";
+
 export const createSubscription = async (req, res, next) => {
     try {
-        const createSubscription = await Subscription.create({
-           ...req.body,
-           user: req.user._id,
+        const subscription = await Subscription.create({
+            ...req.body,
+            user: req.user._id,
         });
-        
-        res.status(201).json({ success: true, data: createSubscription });
+
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription.id,
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            retries: 0,
+        })
+
+        res.status(201).json({ success: true, data: subscription, workflowRunId });
     } catch (error) {
         next(error);
     }
+
 }
 
 export const getUserSubscriptions = async (req, res, next) => {
